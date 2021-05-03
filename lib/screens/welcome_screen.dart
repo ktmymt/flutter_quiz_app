d83a64d0338a463756191ceaca9929cb12930410
ktmymt <kota.miyamoto18@gmail.com>
2021-05-03 17:35:26 +0900

    add welcome message

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quize_app/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Stack(
+      children: [
+        WebsafeSvg.asset("assets/icons/bg.svg"),
+        SafeArea(
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(),
+                    Text(
+                      "Let's Play Quiz",
+                      style: Theme.of(context).textTheme.headline4.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Enter your information below"),
+                    Spacer()
+                  ],
+                )))
+      ],
+    ));
   }
 }
