f47a4ad51e10baa2ae975ed6e4983977ee9e3b92
ktmymt <kota.miyamoto18@gmail.com>
2021-05-03 18:02:23 +0900

    add start button

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -6,37 +6,67 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        WebsafeSvg.asset("assets/icons/bg.svg"),
-        SafeArea(
+      body: Stack(
+        children: [
+          WebsafeSvg.asset("assets/icons/bg.svg"),
+          SafeArea(
             child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Spacer(),
-                    Text(
-                      "Let's Play Quiz",
-                      style: Theme.of(context).textTheme.headline4.copyWith(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Text(
+                    "Let's Play Quiz",
+                    style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Text("Enter your information below"),
+                  Spacer(),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
                     ),
-                    Text("Enter your information below"),
-                    Spacer(),
-                    TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          hintText: "Full Name",
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12)))),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        "Start quiz",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.black),
+                      ),
                     ),
-                    Spacer(),
-                    Spacer()
-                  ],
-                )))
-      ],
-    ));
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
