0afd826e23571636c0e5a37643ba54f7b2dc36a1
ktmymt <kota.miyamoto18@gmail.com>
2021-05-03 18:52:47 +0900

    create body component

---
+++ lib/components/body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quize_app/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        WebsafeSvg.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Column(
+              children: [
+                Container(
+                    width: double.infinity, height: 35, color: Colors.white),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
